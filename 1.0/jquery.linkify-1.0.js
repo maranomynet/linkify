dde00d9a89994156a7c39e1e5cc7d3efa8da8f9d
bt <bt@85455381-3708-0410-a195-d385fbeb4437>
2009-01-02 17:54:19 +0000

    fixed bug which killed whitespace. simplified structure of code by 0.01
    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1518 85455381-3708-0410-a195-d385fbeb4437

--- 1.0/jquery.linkify-1.0.js
+++ 1.0/jquery.linkify-1.0.js
@@ -1,34 +1,32 @@
 (function($){
 
   var url1 = /(^|"|&lt;|\s)(www\..+?\..+?)(\s|&gt;|"|$)/g,
-      url2 = /(^|"|&lt;|\s)(((https?|ftp):\/\/|mailto:).+?)(\s|&gt;|"|$)/g,
+      url2 = /(^|"|&lt;|\s)(((https?|ftp):\/\/|mailto:).+?)(\s|&gt;|"|$)/g;
 
-      linkifyThis = function () {
-        var childNodes = this.childNodes,
-            i = childNodes.length;
-        while(i--)
-        {
-          var n = childNodes[i];
-          if (n.nodeType == 3) {
-            var html = $.trim(n.nodeValue);
-            if (html)
-            {
-              html = html.replace(/&/g, '&amp;')
-                         .replace(/</g, '&lt;')
-                         .replace(/>/g, '&gt;')
-                         .replace(url1, "$1<a href='http://$2'>$2</a>$3")
-                         .replace(url2, "$1<a href='$2'>$2</a>$5");
-              $(n).after(html).remove();
-            }
-          }
-          else if (n.nodeType == 1  &&  !/^(a|button|textarea)$/i.test(n.tagName)) {
-            linkifyThis.call(n);
-          }
-        };
-      };
-  
   $.fn.linkify = function () {
-    return this.each(linkifyThis);
+    return this.each(function () {
+      var childNodes = this.childNodes,
+          i = childNodes.length;
+      while(i--)
+      {
+        var n = childNodes[i];
+        if (n.nodeType == 3) {
+          var html = n.nodeValue;
+          if (/\S/.test(html))
+          {
+            html = html.replace(/&/g, '&amp;')
+                       .replace(/</g, '&lt;')
+                       .replace(/>/g, '&gt;')
+                       .replace(url1, "$1<a href='http://$2'>$2</a>$3")
+                       .replace(url2, "$1<a href='$2'>$2</a>$5");
+            $(n).after(html).remove();
+          }
+        }
+        else if (n.nodeType == 1  &&  !/^(a|button|textarea)$/i.test(n.tagName)) {
+          arguments.callee.call(n);
+        }
+      };
+    });
   };
 
 })(jQuery);
