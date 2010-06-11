3c10dc03462c43932fe962826246daee13470f28
mar <mar.nospam@anomy.net>
2010-06-11 01:59:52 +0000

    Improved plugin handling - plugins are now ensured a clean text-only HTML input.
    NOTE: The live DOM is only updated once per *changed* textNode (to minimize reflows)
    However, even more optimizations are possible by...
    a) avoiding the use of `tmpCont` when no plugins are present, and
    b) by collecting a list of changed nodes and their replacement HTML strings and updating the live DOM afterwards in one fell loop.

--- 1.0/jquery.linkify-1.0.js
+++ 1.0/jquery.linkify-1.0.js
@@ -55,6 +55,13 @@
 
   var noProtocolUrl = /(^|["'(]|&lt;|\s)(www\..+?\..+?)(([:?]|\.+)?(\s|$)|&gt;|[)"',])/g,
       httpOrMailtoUrl = /(^|["'(]|&lt;|\s)(((https?|ftp):\/\/|mailto:).+?)(([:?]|\.+)?(\s|$)|&gt;|[)"',])/g,
+      linkifier = function ( html ) {
+          return html
+                      .replace( noProtocolUrl, '$1<a href="<``>://$2">$2</a>$3' )  // NOTE: we escape `"http` as `"<``>` to make sure `httpOrMailtoUrl` below doesn't find it as a false-positive
+                      .replace( httpOrMailtoUrl, '$1<a href="$2">$2</a>$5' )
+                      .replace( /"<``>/g, '"http' );  // reinsert `"http`
+        },
+
 
       linkify = $.fn.linkify = function ( cfg ) {
           cfg = cfg || {};
@@ -63,17 +70,30 @@
             cfg = { use:cfg };
           }
           var use = cfg.use,
-              plugins = linkify.plugins||{};
+              allPlugins = linkify.plugins || {},
+              plugins = [linkifier],
+              tmpCont;
           if ( use )
           {
             use = $.isArray( use ) ? use : $.trim(use).split( / *, */ );
-            var pluginShortlist = {};
-            for ( var i=0, l=use.length, name;  i<l;  i++ )
+            var plugin,
+                name;
+            for ( var i=0, l=use.length;  i<l;  i++ )
             {
               name = use[i];
-              pluginShortlist[name] = plugins[name];
-            };
-            plugins = pluginShortlist;
+              plugin = allPlugins[name];
+              if ( plugin )
+              {
+                plugins.push( plugin );
+              }
+            }
+          }
+          else
+          {
+            for ( var name in allPlugins )
+            {
+              plugins.push( allPlugins[name] );
+            }
           }
 
           return this.each(function () {
@@ -85,29 +105,43 @@
                 if ( n.nodeType == 3 )
                 {
                   var html = n.nodeValue;
-                  if ( /\S/.test(html) )
+                  if ( html.length>1  &&  /\S/.test(html) )
                   {
-                    html = html
-                              .replace( /&/g, '&amp;' )
-                              .replace( /</g, '&lt;' )
-                              .replace( />/g, '&gt;' );
-                    var preHtml = html;
-                    html = html
-                              .replace( noProtocolUrl, '$1<a href="<``>://$2">$2</a>$3' )  // NOTE: we escape `"http` as `"<``>` to make sure `httpOrMailtoUrl` below doesn't find it as a false-positive
-                              .replace( httpOrMailtoUrl, '$1<a href="$2">$2</a>$5' )
-                              .replace( /"<``>/g, '"http' );  // reinsert `"http`
-                    
-                    for ( var name in plugins )
+                    var htmlChanged,
+                        preHtml;
+                    tmpCont = tmpCont || $('<div/>')[0];
+                    tmpCont.innerHTML = '';
+                    tmpCont.appendChild( n.cloneNode(false) );
+                    var tmpContNodes = tmpCont.childNodes;
+
+                    for (var j=0, plugin; (plugin = plugins[j]); j++)
                     {
-                      var plugin = plugins[name] || {};
-                      html = $.isFunction( plugin ) ? 
-                                  plugin( html ):
-                                  html.replace( plugin.re, plugin.tmpl );
+                      var k = tmpContNodes.length,
+                          tmpNode;
+                      while ( k-- )
+                      {
+                        tmpNode = tmpContNodes[k];
+                        if ( tmpNode.nodeType == 3 )
+                        {
+                          html = tmpNode.nodeValue;
+                          if ( html.length>1  &&  /\S/.test(html) )
+                          {
+                            preHtml = html;
+                            html = html
+                                      .replace( /&/g, '&amp;' )
+                                      .replace( /</g, '&lt;' )
+                                      .replace( />/g, '&gt;' );
+                            html = $.isFunction( plugin ) ? 
+                                        plugin( html ):
+                                        html.replace( plugin.re, plugin.tmpl );
+                            htmlChanged = htmlChanged || preHtml!=html;
+                            preHtml!=html  &&  $(tmpNode).after(html).remove();
+                          }
+                        }
+                      }
                     }
-                    if (html != preHtml)
-                    {
-                      $(n).after(html).remove();
-                    }
+                    html = tmpCont.innerHTML;
+                    htmlChanged  &&  $(n).after(html).remove();
                   }
                 }
                 else if ( n.nodeType == 1  &&  !/^(a|button|textarea)$/i.test(n.tagName) )
